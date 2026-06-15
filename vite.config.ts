775d220f044ca18da3d94cccee46202c65928185
Maisone Developer <developer@maisone.com>
2026-06-15 14:54:44 +0530

    Add React Vite plugin to resolve React Refresh runtime error in dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
     tanstackStart(),
     tsConfigPaths(),
